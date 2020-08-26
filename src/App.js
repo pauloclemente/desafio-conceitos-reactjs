20103cf2286a7f5ae262f588549f71dad48bb02b
Paulo Barros <clementedbarros@gmail.com>
2020-08-26 17:20:56 -0300

    feat: add and remove repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
-
 import "./styles.css";
+import { useEffect } from "react";
+import api from "./services/api";
+import { useState } from "react";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(()=> {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  },[])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Snooze',
+      url: 'https://github.com/pauloclemente/snooze',
+      techs: ['Node.js', 'React Native', 'Typescript']
+    })
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      rep => rep.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(rep => (
+          <li key={rep.id}>
+          {rep.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(rep.id)}>
             Remover
           </button>
         </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
